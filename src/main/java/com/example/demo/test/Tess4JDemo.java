d1f01ab9806d0390de599700c3da125e51600734
wuxiangen@gtland.cn <wxe@2019>
2020-05-13 14:55:36 +0800

    调整

--- src/main/java/com/example/demo/test/Tess4JDemo.java
+++ src/main/java/com/example/demo/test/Tess4JDemo.java
@@ -9,7 +9,6 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.awt.*;
 
 public class Tess4JDemo {
 
@@ -53,59 +52,5 @@ public class Tess4JDemo {
         }
 
         System.out.println("result: " + result);
-    }
-
-
-    public static void removeBackground(String imgUrl, String resUrl) {
-        //定义一个临界阈值
-        int threshold = 300;
-        try {
-            BufferedImage img = ImageIO.read(new File(imgUrl));
-            int width = img.getWidth();
-            int height = img.getHeight();
-            for (int i = 1; i < width; i++) {
-                for (int x = 0; x < width; x++) {
-                    for (int y = 0; y < height; y++) {
-                        Color color = new Color(img.getRGB(x, y));
-                        System.out.println("red:" + color.getRed() + " | green:" + color.getGreen() + " | blue:" + color.getBlue());
-                        int num = color.getRed() + color.getGreen() + color.getBlue();
-                        if (num >= threshold) {
-                            img.setRGB(x, y, Color.WHITE.getRGB());
-                        }
-                    }
-                }
-            }
-            for (int i = 1; i < width; i++) {
-                Color color1 = new Color(img.getRGB(i, 1));
-                int num1 = color1.getRed() + color1.getGreen() + color1.getBlue();
-                for (int x = 0; x < width; x++) {
-                    for (int y = 0; y < height; y++) {
-                        Color color = new Color(img.getRGB(x, y));
-
-                        int num = color.getRed() + color.getGreen() + color.getBlue();
-                        if (num == num1) {
-                            img.setRGB(x, y, Color.BLACK.getRGB());
-                        } else {
-                            img.setRGB(x, y, Color.WHITE.getRGB());
-                        }
-                    }
-                }
-            }
-            File file = new File(resUrl);
-            if (!file.exists()) {
-                File dir = file.getParentFile();
-                if (!dir.exists()) {
-                    dir.mkdirs();
-                }
-                try {
-                    file.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            ImageIO.write(img, "jpg", file);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
